eed12b71bf67adf514f54e5a4fdfbe1b18b1dc48
merlight <merlight@users.noreply.github.com>
2015-03-11 11:50:10 +0100

    fix saving guild member/friend note

--- merBandAidZouiFixes.lua
+++ merBandAidZouiFixes.lua
@@ -25,3 +25,25 @@ if STORE_WINDOW_GAMEPAD and STORE_WINDOW_GAMEPAD.RepairMessageBox then
         return
     end
 end
+
+
+--[======================================[
+    error message upon guild note change
+    ------------------------------------
+    http://www.esoui.com/forums/showthread.php?t=4384
+
+    [circonian] Oh I was doing it wrong, I was able to reproduce it.
+    I was right clicking on my name & clicking "Edit Note" I didn't realize there was a note button to click on.
+    If I click on the note button next to my name & do what you said I do get the error.
+
+--]======================================]
+
+-- fix by circonian
+
+ZO_PreHook("ZO_KeyboardGuildRosterRowNote_OnClicked", function(control)
+    GUILD_ROSTER_KEYBOARD.noteEditedFunction = GUILD_ROSTER_MANAGER:GetNoteEditedFunction()
+end)
+
+ZO_PreHook("ZO_FriendsListRowNote_OnClicked", function(control)
+    FRIENDS_LIST.noteEditedFunction = FRIENDS_LIST_MANAGER:GetNoteEditedFunction()
+end)
