9c60965bcccb63102fb3267bb8950244414978df
merlight <merlight@users.noreply.github.com>
2015-09-14 17:00:35 +0200

    remove custom UI scale fix, obsolete in ESO 2.1.5

--- merBandAidZouiFixes.lua
+++ merBandAidZouiFixes.lua
@@ -1,3 +1,4 @@
+--[[
 local myNAME = "merBandAidZouiFixes"
 local EM = EVENT_MANAGER
 local SV = {}
@@ -11,30 +12,4 @@ local function onLoaded(eventCode, addonName)
 end
 
 EM:RegisterForEvent(myNAME, EVENT_ADD_ON_LOADED, onLoaded)
-
-
---[====================================================================[
-    Restore custom UI scale after login or zone change.
-
-    http://forums.elderscrollsonline.com/en/discussion/212138/ui-scaling-resetting
---]====================================================================]
-
-
-local function onScreenResized(eventCode, x, y, guiName)
-    -- this event is triggered by changing the custom scale or screen resolution;
-    -- store current custom scale setting in ZO_Ingame_SavedVariables
-    SV.UI_SETTING_CUSTOM_SCALE = GetSetting(SETTING_TYPE_UI, UI_SETTING_CUSTOM_SCALE)
-end
-
-local function onPlayerActivated(eventCode)
-    local currentScale = tonumber(GetSetting(SETTING_TYPE_UI, UI_SETTING_CUSTOM_SCALE))
-    local savedScale = tonumber(SV.UI_SETTING_CUSTOM_SCALE)
-    if currentScale and savedScale and math.abs(currentScale - savedScale) >= 1e-5 then
-        --df("restoring UI scale because diff = %.6f", math.abs(currentScale - savedScale))
-        SetSetting(SETTING_TYPE_UI, UI_SETTING_CUSTOM_SCALE, SV.UI_SETTING_CUSTOM_SCALE)
-        ApplySettings()
-    end
-end
-
-EM:RegisterForEvent(myNAME, EVENT_PLAYER_ACTIVATED, onPlayerActivated)
-EM:RegisterForEvent(myNAME, EVENT_SCREEN_RESIZED, onScreenResized)
+--]]
